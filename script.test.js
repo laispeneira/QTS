17adc910cfe16a1e05f7e845ade219b3549e5e9b
laispeneira <llaispereiraa2303@gmail.com>
2026-05-25 08:49:18 -0300

    commit ex

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { maiorNumero, mensagemBoasVindas, somar } = require('./script');
+const { maiorNumero, mensagemBoasVindas, somar, ImparPar, Votar, Numeros, Bonus, Dirigir, Semestre } = require('./script');
 
 test('maior numero', () => {
   expect(maiorNumero(2, 3)).toBe(3);
@@ -10,3 +10,25 @@ test('mensagem de boas vindas', () => {
 test('somar dois numeros', () => {
   expect(somar(2, 3)).toBe(5);
 });
+test('numero par ou impar', () => {
+  expect(ImparPar(2, 3)).toBe('O primeiro numero é par: 2', 'O segundo numero é impar: 3');
+});
+test('condição votar', () => {
+  expect(Votar(15)).toBe('Voce não pode votar');
+});
+test('condição numeros', () => {
+  expect(Numeros(-2)).toBe('Numero negativo');
+});
+test('condição bonus', () => {
+  expect(Bonus(5000)).toBe('Seu bônus é de: 500 e o total ficará 5500');
+});
+test('condição dirigir', () => {
+  expect(Dirigir(18)).toBe('Voce pode dirigir');
+});
+test('semestre', () => {
+  expect(Semestre('janeiro')).toBe('primeiro semestre');
+});
+
+
+
+
---
+++ usuarioService.js
@@ -0,0 +1,12 @@
+function buscarUsuarioNoBanco(id) {
+    // Imagine que isso acessa banco/API
+    return { id, nome: 'João Mockado' };
+  }
+  
+  module.exports = { buscarUsuarioNoBanco };
+  function buscarUsuarioNoBanco(id) {
+    // Imagine que isso acessa banco/API
+    return { id, nome: 'Maria' };
+  }
+  
+  module.exports = { buscarUsuarioNoBanco };
